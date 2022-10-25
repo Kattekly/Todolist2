894a26b1200d6b39724715f8705a8c570e5fdbb4
Ekaterina <kattekly@mail.ru>
2022-10-25 18:30:46 +0300

    Lessons-7

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
-import {Button} from "@mui/material";
+import Button from "@mui/material/Button";
 
 type InputPropsType = {
     callback: (title: string) => void
@@ -39,7 +39,8 @@ export const Input = (props: InputPropsType) => {
                    onKeyPress={onKeyPressHandler}
                    className={error ? "error" : ""}
             />
-            <Button onClick={addTask}>+</Button>
+            <Button  variant="contained" style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}
+                     onClick={addTask}>+</Button>
             {error && <div className="error-message">{error}</div>}
         </div>
     );
