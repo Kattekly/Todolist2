e287127551e313a7a378a848d1a78dfcc5d88472
Ekaterina <kattekly@mail.ru>
2022-10-25 15:26:38 +0300

    Lessons-7

---
+++ src/components/Input.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Button} from "@mui/material";
+
+type InputPropsType = {
+    callback: (title: string) => void
+}
+
+
+export const Input = (props: InputPropsType) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.key === "Enter") {
+            addTask();
+        }
+    }
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.callback(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+
+    return (
+        <div>
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? "error" : ""}
+            />
+            <Button onClick={addTask}>+</Button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    );
+};
