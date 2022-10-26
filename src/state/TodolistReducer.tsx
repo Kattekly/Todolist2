98f9d7dbf8338b708069d84658acd1bd82c80b32
Ekaterina <kattekly@mail.ru>
2022-10-26 10:35:09 +0300

    Lessons-8

---
+++ src/state/TodolistReducer.tsx
@@ -0,0 +1,10 @@
+import {TodolistType} from "../App";
+
+export const todolistReducer = (state: Array<TodolistType>, action: any) => {
+    switch (action.type) {
+        case '' {
+
+        }
+        default: return state
+    }
+}
