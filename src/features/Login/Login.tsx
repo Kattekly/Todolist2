7ca3b88945c012aad21dfd86c2998094f0203d23
Ekaterina <kattekly@mail.ru>
2022-12-29 20:23:26 +0300

    Lessons-16

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -60,27 +60,30 @@ export const Login = () => {
 
                         <TextField label="Email"
                                    margin="normal"
-                                   name={'email'}
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.email}/>
+                            // name={'email'}
+                            // onChange={formik.handleChange}
+                            // onBlur={formik.handleBlur}
+                            // value={formik.values.email}/>
+                                   {...formik.getFieldProps('email')} />
                         {formik.touched.email && formik.errors.email &&
                             <div style={{color: "red"}}>{formik.errors.email}</div>}
 
 
                         <TextField type="password" label="Password"
                                    margin="normal"
-                                   name={'password'}
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
-                                   value={formik.values.password}
+                            /*name={'password'}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}*/
+                                   {...formik.getFieldProps('password')}
                         />
                         {formik.touched.password && formik.errors.password &&
                             <div style={{color: "red"}}>{formik.errors.password}</div>}
                         <FormControlLabel label={'Remember me'}
-                                          control={<Checkbox name={'rememberMe'}
+                                          control={<Checkbox /*name={'rememberMe'}
                                                              onChange={formik.handleChange}
-                                                             value={formik.values.rememberMe}/>}
+                                                             value={formik.values.rememberMe}*/
+                                              {...formik.getFieldProps('rememberMe')}/>}
                         />
                         <Button type={'submit'} variant={'contained'} color={'primary'}>
                             Login
