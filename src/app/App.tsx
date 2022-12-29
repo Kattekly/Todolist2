a84f4044f6d5db54874e5ea07fc1b270cf13a23d
Ekaterina <kattekly@mail.ru>
2022-12-29 18:07:45 +0300

    Lessons-16

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,8 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 
 function App() {
@@ -32,6 +34,10 @@ function App() {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
+
+                    <TodolistsList />
+                    <Login/>
+
                 <TodolistsList/>
             </Container>
         </div>
