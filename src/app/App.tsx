7ad8abd9b72485b5e95b2e84b697edc2b91ae345
Ekaterina <kattekly@mail.ru>
2022-12-30 09:22:57 +0300

    Lessons-16

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -47,7 +47,6 @@ function App() {
                     <Route path='/404' element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
                     <Route path='*' element={<Navigate to={'/404'}/>}/>
                 </Routes>
-                <TodolistsList/>
             </Container>
         </div>
     )
